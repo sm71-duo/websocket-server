c027b01e6e7917f8cace4dffff258b257e09fdf2
royappeldoorn <roy.appeldoorn@gmail.com>
2021-09-10 15:33:17 +0200

    first setup for rooms

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,43 @@
-import { Server } from 'ws';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import express from 'express';
 
-const wss = new Server({ port: 3000 });
+const app = express();
+const httpServer = createServer(app);
 
-wss.on('connection', (ws, req) => {
-  if (ws.readyState === ws.OPEN) {
-    const ip = req.socket.remoteAddress;
+const io = new Server(httpServer);
 
-    ws.send(
-      JSON.stringify({
-        message: `Client with IP address ${ip} Connected to the server`,
-      })
-    );
+const rooms: any[] = [];
 
-    ws.on('message', (message) => {
-      console.log('received: %s', message);
-      ws.send(
-        JSON.stringify({
-          message: 'Message from the server!',
-        })
-      );
-    });
-  }
+io.on('connection', (socket: Socket) => {
+  socket.on('join room', (roomId) => {
+    if (rooms[roomId]) {
+      rooms[roomId].push(socket.id);
+    } else {
+      rooms[roomId] = [socket.id];
+    }
+
+    const otherUser = rooms[roomId].find((id: string) => id !== socket.id);
+    if (otherUser) {
+      socket.emit('other user', otherUser);
+      socket.to(otherUser).emit('user joined', socket.id);
+    }
+  });
+
+  socket.on('offer', (payload) => {
+    io.to(payload.target).emit('offer', payload);
+  });
+
+  /*
+    The receiving peer answers (accepts) the offer
+  */
+  socket.on('answer', (payload) => {
+    io.to(payload.target).emit('answer', payload);
+  });
+
+  socket.on('ice-candidate', (incoming) => {
+    io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+  });
 });
+
+httpServer.listen(3000);
